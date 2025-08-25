3533ab16a97e24970b0ec0db646ee7cb2deb7128
Lusteenyne <sakaoluwasegun26@gmail.com>
2025-08-25 14:57:43 +0100

    a new schhol

--- src/components/NewLanding.jsx
+++ src/components/NewLanding.jsx
@@ -3,7 +3,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import './Newlanding.css';
+import './NewLanding.css';
 
 import Slider from 'react-slick';
 
